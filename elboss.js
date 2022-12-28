3d7c7d96744662603f9d234b7d7752f9aa0ccc1b
Alejandro <frozmiz@live.com>
2022-12-28 01:06:59 +0100

    feat

--- elboss.js
+++ elboss.js
@@ -77,7 +77,7 @@ const elBoss = {
     [
       {
         sigueElReto: [
-          {
+         { 
             sigueElReto2: {
               sigueElReto3: {
                 pareceQueSabesBucearEntreObjetos: {
@@ -124,3 +124,37 @@ console.log(`tu primer poder es ${poder1.nombre} y hace ${poder1.description}`);
 console.log(`tu primer poder es ${poder2.nombre} y hace ${poder2.description}`);
 console.log(`tu primer poder es ${poder3.nombre} y hace ${poder3.description}`);
 
+// 4: le vamos a decir "También se tu punto más debil, tus familiares, y les conozco..."
+// "conozco a tu {parentesco del primer familiar} {nombre del primer familiar}"
+// "conozco a tu {parentesco del segundo familiar} {nombre del segundo familiar}"
+// "conozco a tu {parentesco del tercero familiar} {nombre del tercero familiar}"
+// "y conozco a tu {parentesco del cuarto familiar} {nombre del cuarto familiar}"
+
+const [fam1, fam2, fam3, fam4] = familiares;
+
+console.log(`conozco a tu ${fam1.parentesco} ${fam1.nombre}`);
+console.log(`conozco a tu ${fam2.parentesco} ${fam2.nombre}`);
+console.log(`conozco a tu ${fam3.parentesco} ${fam3.nombre}`);
+console.log(`conozco a tu ${fam4.parentesco} ${fam4.nombre}`);
+
+// 5: para matarlo tendrás que superar el reto y para ello tendrás que decirle las palabras
+// mágicas para que el mensaje quede tal que así "Jose luis, te miro y te destruyo".
+
+const [reto1] = reto;
+// const [reto2] = reto1;
+
+
+for (const retito of reto1) {
+    for (const key in retito.sigueElReto) {
+        console.log(retito.sigueElReto[key].sigueElReto2);
+        
+    }
+}
+
+
+
+
+
+
+// console.log(reto1);
+// console.log(reto2);
